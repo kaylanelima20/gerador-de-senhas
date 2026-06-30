c22d2848e124c715d5bd16299c531de62330ebe2
kaylane <kaylanesattilo2005@gmail.com>
2026-06-30 19:40:52 -0300

    adicionando os checkboxes no javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function generatePassword(){
+    const length = document.getElementById("length").value;
+
+    const uppercase = document.getElementById("uppercase").Checked;
+    const lowercase = document.getElementById("lowercase").Checked;
+    const numbers = document.getElementById("numbers").Checked;
+    const symbols = document.getElementById("symbols").Checked;
+
+}
